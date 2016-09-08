64250101cf6975245ce1461710c7d42639bb58bb
mattga <mgardner091@gmail.com>
2016-09-08 12:11:51 -0500

    Update apis.js

--- js/apis.js
+++ js/apis.js
@@ -60,7 +60,7 @@ $(document).ready(function(){
             }
         }
     });
-    mgchrating.prepend('<h6>344</h6>');
+    mgchrating.prepend('<h6>600</h6>');
     mgchrating.prepend('<i class="stars fa fa-download" aria-hidden="true"></i>');
 
     $.ajax({
@@ -76,7 +76,7 @@ $(document).ready(function(){
             }
         }
     });
-    mgaurating.prepend('<h6>13</h6>');
+    mgaurating.prepend('<h6>27</h6>');
     mgaurating.prepend('<i class="stars fa fa-download" aria-hidden="true"></i>');
 
     // $.ajax({
